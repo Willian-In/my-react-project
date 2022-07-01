c63a9976c73e85a00657377d2fd97ae8ce7237c6
写个Bug孝敬宁 <1141026042@qq.com>
2022-07-01 19:31:10 +0800

    优化路由, 用数组进行管理, 并使用路由懒加载来加载路由

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,23 +1,49 @@
+import { lazy, Suspense } from 'react'
 import { BrowserRouter, Routes,  Route } from 'react-router-dom'
 import App from 'App'
-import Login from 'Login'
-import Register from 'Register'
-import List from 'pages/List'
+// import Login from 'Login'
+// import Register from 'Register'
+// import List from 'pages/List'
+
+
+// 组件懒加载
+const router_arr = [
+  {path: '/', component: App, children: [
+    // 引入子页面
+    {path: '/list', component: lazy(() => import("pages/List"))},
+  ]},
+  // 同级页面
+  {path: '/login', component: lazy(() => import("Login"))},
+  {path: '/register', component: lazy(() => import("Register"))}
+]
+
+
 
 // 配置项目路由
 const BaseRouter = (): any => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route path='/list' element={<List />}></Route>
-        </Route>
-        <Route path='/login' element={<Login />}></Route>
-        <Route path='/register' element={<Register />}></Route>
-      </Routes>
+    {/* 路由加载需要等待动画 */}
+      <Suspense fallback={<div>loading....</div>}>
+        <Routes>
+          {
+            router_arr.map((item, index) => {
+              return (
+                item.children ? // 有子路由
+                <Route key={index} path={item.path} element={<item.component />}>
+                  {
+                    item.children.map((e, i) => <Route key={i} path={e.path} element={<e.component />}></Route>)
+                  }
+                </Route>
+                :// 没有自由路
+                <Route key={index} path={item.path} element={<item.component />}></Route>
+              )
+            })
+          }
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   )
-  
 }
 
 export default BaseRouter
